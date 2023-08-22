2cf5d3fd16f471e07e73d1e105a403aee85c725d
Francisco Casanova <francisco.casanovah@carabineros.cl>
2023-08-22 09:54:13 -0400

    Se crea modulo de ingreso de licencias medicas

---
+++ src/app/pages/ingreso/ingreso.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { IngresoRoutingModule } from './ingreso-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    IngresoRoutingModule
+  ]
+})
+export class IngresoModule { }
